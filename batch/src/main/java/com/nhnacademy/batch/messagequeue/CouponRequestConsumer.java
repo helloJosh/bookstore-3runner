be45dbaede776edd0ee849ca398cfcac9d2b6eb7
helloJosh <kimjoshua135@gmail.com>
2025-11-29 17:53:20 +0900

    Update CouponRequestConsumer.java

--- batch/src/main/java/com/nhnacademy/batch/messagequeue/CouponRequestConsumer.java
+++ batch/src/main/java/com/nhnacademy/batch/messagequeue/CouponRequestConsumer.java
@@ -19,7 +19,7 @@ public class CouponRequestConsumer {
     private static final String COUNT_KEY = "req:{%s}:count";
     private static final String LIST_KEY  = "req:{%s}:list";
 
-    @RabbitListener(queues = "q.create.request") // 기본 auto-ack 가정
+    @RabbitListener(queues = "q.create.request")
     public void onMessage(String msg) {
         log.info("q.create.request : {} consumer recieved msg", msg);
         long now = RedisTime.nowMillis(redis);
@@ -28,13 +28,11 @@ public class CouponRequestConsumer {
         String countKey = COUNT_KEY.formatted(slot);
         String listKey  = LIST_KEY.formatted(slot);
 
-        // 1) 카운트 증가
         Long v = redis.opsForValue().increment(countKey);
         if (v != null && v == 1L) {
             redis.expire(countKey, Duration.ofDays(3));
         }
 
-        // 2) 메시지 리스트에 적재
         redis.opsForList().rightPush(listKey, msg);
         if (v != null && v == 1L) {
             redis.expire(listKey, Duration.ofDays(3));
